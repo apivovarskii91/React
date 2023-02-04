ede222116c45063625bc1ed6e1d675c246f21990
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-04 03:01:46 +0200

    remove files

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './Header'
-import Content from './Content'
-import List from './List'
-
-const App = () => {
-    return (
-        <>
-            <Header title="React" year={2023} />
-            <Header title="TS" />
-            <Content
-                text1="Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Debitis rerum ea eveniet laudantium porro iure, repudiandae quis
-                sunt voluptate repellendus optio facere rem modi totam non
-                eligendi fuga a inventore."
-                text2=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-                accusantium impedit eum voluptatibus ad qui pariatur quae, eius
-                libero nulla quisquam harum saepe quidem mollitia enim molestiae
-                reprehenderit quasi beatae?"
-            />
-            <List item1="App" item2="React" item3="TS" />
-        </>
-    )
-} // компоненти
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
