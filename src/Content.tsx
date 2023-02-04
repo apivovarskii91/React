1355ca4e36756f598c90d20fa8eeac47a18e6f5a
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-04 02:22:32 +0200

    надання стилів через импорт

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,21 +1,13 @@
+import './Content.css'
 type ContentProps = {
     text1: string
     text2: string
 }
 
 const Content = ({ text1, text2 }: ContentProps) => {
-    let data = false
     return (
         <>
-            <p
-                style={{
-                    backgroundColor: data ? 'purple' : 'green',
-                    color: 'white',
-                    padding: '15px',
-                }}
-            >
-                {text1}
-            </p>
+            <p className="content">{text1}</p>
             <p>{text2}</p>
         </>
     )
