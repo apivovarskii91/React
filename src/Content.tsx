37c2c3fbb6b7f82512acad6984be6d60846b6cf5
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-04 02:47:45 +0200

    деструктуризація classes

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,14 +1,16 @@
-import './Content.css'
+import classes from './Content.module.css'
+const { content, purple, peru } = classes
+
 type ContentProps = {
     text1: string
     text2: string
 }
 
 const Content = ({ text1, text2 }: ContentProps) => {
-    let date = false
+    let date = true
     return (
         <>
-            <p className={`content ${date ? 'purple' : 'peru'}`}>{text1}</p>
+            <p className={`${content} ${date ? purple : peru}`}>{text1}</p>
             <p>{text2}</p>
         </>
     )
