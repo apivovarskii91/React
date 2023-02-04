fdb20f88fcc03bee2ce4bf88df95cb141b2efb1c
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-04 19:41:28 +0200

    add ProductList

---
+++ src/components/ProductsList/ProductsList.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+
+const ProductsList = (props: Props) => {
+    return (
+        <>
+            <Typography component={'h2'} variant="h4" align="center">
+                Products List
+            </Typography>
+        </>
+    )
+}
+export default ProductsList
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import ProductsList from 'components/ProductsList/ProductsList'
+
+type Props = {}
+const Home = (props: Props) => {
+    return (
+        <>
+            <ProductsList />
+        </>
+    )
+}
+export default Home
