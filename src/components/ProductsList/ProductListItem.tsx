cb3afdd134060f8abb466efa61a635e445e38ef2
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-05 01:00:46 +0200

    add props to ProductListItem

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -1,16 +1,24 @@
 import { Button, Card, CardContent, CardActions } from '@mui/material'
 import './ProductsListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 const ProductListItem = (props: Props) => {
     return (
         <Card variant="outlined" className="product">
             <CardContent>
-                <div className="product-title">iPhone 14 Pro</div>
-                <div className="product-description">This iPhone 14 Pro</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity: 256 Gb</div>
-                <div className="product-features">Price: 1500$</div>
+                <div className="product-title">{props.title}</div>
+                <div className="product-description">{props.description}</div>
+                <div className="product-features">Type:{props.type}</div>
+                <div className="product-features">
+                    Capacity:{props.capacity}Gb
+                </div>
+                <div className="product-features">Price:{props.price}$</div>
             </CardContent>
             <CardActions className="btns-wrap">
                 <Button variant="outlined">Add to cart</Button>
