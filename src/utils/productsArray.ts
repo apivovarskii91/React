c67f9eef22682caca8ff070c1e68c3febfe1e8c8
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-05 01:12:27 +0200

    add props to Product Price

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,56 @@
+type Product = {
+  title: string
+  description: string
+  type: string
+  capacity: string
+  price: number
+}
+
+
+const productsArray: Product[] = [
+  {
+  title:"iPhone 14 Pro",
+  description:"This is iPhone 14 Pro",
+  type:"phone",
+  capacity:"256",
+  price:1500,
+},
+{
+  title:"iPhone 13 Pro",
+  description:"This is iPhone 13 Pro",
+  type:"phone",
+  capacity:"512",
+  price:1000,
+},
+{
+  title:"iPhone 12 Pro",
+  description:"This is iPhone 12 Pro",
+  type:"phone",
+  capacity:"128",
+  price:500,
+},
+{
+  title:"iPhone X ",
+  description:"This is iPhone X",
+  type:"phone",
+  capacity:"64",
+  price:500,
+},
+{
+  title:"iPhone 14 Pro Max",
+  description:"This is iPhone 14 Pro Max",
+  type:"phone",
+  capacity:"512",
+  price:2000,
+},
+{
+  title:"iPhone XS",
+  description:"This is iPhone XS",
+  type:"phone",
+  capacity:"256",
+  price:1000,
+},
+]
+
+
+export default productsArray
