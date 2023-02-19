ca313adba3f2fdd5e881209b9137f3cefc41e762
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-19 14:14:39 +0200

    producrID -1 title

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,14 +1,18 @@
+import productsArray from 'utils/productsArray'
+
 type Props = {
     productsInCart: {
         [id: number]: number
     }
 }
+
 const CartHeader = ({ productsInCart }: Props) => {
     return (
         <div>
             {Object.keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productId}: {productsInCart[parseInt(productId)]}
+                    {productsArray[parseInt(productId) - 1].title}:{' '}
+                    {productsInCart[parseInt(productId)]}
                 </div>
             ))}
         </div>
