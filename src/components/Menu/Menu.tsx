f5c4079d40d108b9b545e915fb5ba28a688ed5e3
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-25 14:21:06 +0200

    add link menu string

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button'
 
 type Props = {}
@@ -5,11 +6,21 @@ type Props = {}
 const Menu = (props: Props) => {
     return (
         <>
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit">Payment</Button>
-            <Button color="inherit">Shipping</Button>
-            <Button color="inherit">Cart</Button>
+            <Button color="inherit">
+                <Link to="/">Home</Link>
+            </Button>
+            <Button color="inherit">
+                <Link to="/about">About</Link>
+            </Button>
+            <Button color="inherit">
+                <Link to="/payment">Payment</Link>
+            </Button>
+            <Button color="inherit">
+                <Link to="/shipping">Shipping</Link>
+            </Button>
+            <Button color="inherit">
+                <Link to="/cart">Cart</Link>
+            </Button>
         </>
     )
 }
