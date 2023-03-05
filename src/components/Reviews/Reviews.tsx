e140d9aa1af696151160271f88e4ca15bea3e837
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-03-05 13:46:08 +0200

    add set NewReviews

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,10 +1,17 @@
+import {
+    Button,
+    Card,
+    CardContent,
+    TextareaAutosize,
+    TextField,
+} from '@mui/material'
 import Typography from '@mui/material/Typography'
 import { type } from 'os'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
-type Reviews = {
+type Review = {
     name: string
     text: string
 }
@@ -20,18 +27,71 @@ const Reviews = (props: Props) => {
         },
     ]
 
-    const [reviews, setReviews] = useState<Reviews[]>(arrReviews)
+    const [reviews, setReviews] = useState<Review[]>(arrReviews)
+
+    const [newReview, setNewReview] = useState<Review>({
+        name: '',
+        text: '',
+    })
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+    }
+    const handleText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+    }
+    const sendReview = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setReviews((prevState) => {
+            return [...prevState, newReview]
+        })
+
+        setNewReview({
+            name: '',
+            text: '',
+        })
+    }
     return (
         <div>
             <Typography variant="h4" component="h2" sx={{ margin: '30px 0' }}>
                 Reviews
             </Typography>
-            {reviews.map((item) => (
-                <div>
-                    <div>{item.name}:</div>
-                    <div>{item.text}</div>
-                </div>
+            {reviews.map((item, i) => (
+                <Card variant="outlined" sx={{ margin: '30px 0' }} key={i}>
+                    <CardContent>
+                        <div>{item.name}:</div>
+                        <div>{item.text}</div>
+                    </CardContent>
+                </Card>
             ))}
+            <form onSubmit={sendReview}>
+                <h3>Please leave a review</h3>
+                <div>
+                    <TextField
+                        label="Your name"
+                        value={newReview.name}
+                        onChange={handleName}
+                    />
+                </div>
+                <br />
+                <div>
+                    <TextareaAutosize
+                        minRows={7}
+                        placeholder="Your text"
+                        value={newReview.text}
+                        onChange={handleText}
+                    />
+                </div>
+                <Button variant="outlined" type="submit">
+                    Send
+                </Button>
+            </form>
         </div>
     )
 }
